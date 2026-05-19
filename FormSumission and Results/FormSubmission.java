f87746c17a8d4279d479217a0d26b2da4c6394ff
Sacosta29 <samuel.acosta2005@icloud.com>
2026-05-19 15:52:11 -0400

    Update FormSubmission.java

--- FormSumission and Results/FormSubmission.java
+++ FormSumission and Results/FormSubmission.java
@@ -1,3 +1,12 @@
 public class FormSubmission {
-    
+
+    private double budget;
+    private int timeline;
+    private String riskTolerance;
+
+    public FormSubmission(double budget, int timeline, String riskTolerance) {
+        this.budget = budget;
+        this.timeline = timeline;
+        this.riskTolerance = riskTolerance;
+    }
 }
