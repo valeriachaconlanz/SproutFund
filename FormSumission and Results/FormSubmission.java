ab25485a0385a55816274dca49bb3e93220abfa3
Sacosta29 <samuel.acosta2005@icloud.com>
2026-05-19 16:23:25 -0400

    Update FormSubmission.java

--- FormSumission and Results/FormSubmission.java
+++ FormSumission and Results/FormSubmission.java
@@ -9,4 +9,39 @@ public class FormSubmission {
         this.timeline = timeline;
         this.riskTolerance = riskTolerance;
     }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public int getTimeline() {
+        return timeline;
+    }
+
+    public String getRiskTolerance() {
+        return riskTolerance;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public void setTimeline(int timeline) {
+        this.timeline = timeline;
+    }
+
+    public void setRiskTolerance(String riskTolerance) {
+        this.riskTolerance = riskTolerance;
+    }
+
+    public void submitForm() {
+        System.out.println("Form Successfully Submitted!");
+        System.out.println("Budget: $" + budget);
+        System.out.println("Timeline: " + timeline);
+        System.out.println("Risk Tolerance: " + riskTolerance);
+    }
+    
+    public void resultsRedirect() {
+        System.out.println("Redirecting to investment results page...");
+    }
 }
